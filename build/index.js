4c6e4357005f598bdf99e91d81eed1dc8b231897
rajatcantcode <rajatcantcode@gmail.com>
2024-07-18 13:31:58 +0530

    completed

--- build/index.js
+++ build/index.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("Rajat");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const snippet = yield prisma.snippet.create({
+            data: {
+                title: "Hello World",
+                code: "console.log('Hello World')",
+            },
+        });
+        console.log(snippet); //{ id: 1, title: 'Hello World', code: "console.log('Hello World')" }
+    });
+}
+main()
+    .catch((e) => {
+    console.log(e.message);
+})
+    .finally(() => __awaiter(void 0, void 0, void 0, function* () { return yield prisma.$disconnect(); }));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Rajat");
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const snippet = await prisma.snippet.create({
+    data: {
+      title: "Hello World",
+      code: "console.log('Hello World')",
+    },
+  });
+
+  console.log(snippet); //{ id: 1, title: 'Hello World', code: "console.log('Hello World')" }
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => await prisma.$disconnect());
